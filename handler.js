af29d0806cf7bcf7d22e13a75025df28c981dae8
unknown <aagarces@uninorte.edu.co>
2021-06-21 20:32:15 -0500

    First Push

---
+++ handler.js
@@ -0,0 +1,95 @@
+const db = require('./db_connect');
+'use strict';
+
+module.exports.getUsers= (event,context,callback) => {
+  context.callbackWaitsForEmptyEventLoop = false;
+
+  db.getById('users', event.pathParameters.id)
+    .then(res => {
+      callback(null,{
+        statusCode: 200,
+        body: JSON.stringify(res)
+      })
+    })
+    .catch(e => {
+      callback(null,{
+        statusCode: e.statusCode || 500,
+        body: "Could not find users: " + e
+      })
+    })
+};
+
+module.exports.getAllUsers = (event,context,callback) => {
+  context.callbackWaitsForEmptyEventLoop = false;
+
+  db.getAll('users')
+    .then(res => {
+      callback(null, {
+        statusCode: 200,
+        body: JSON.stringify(res)
+      })
+    })
+    .catch(e => {
+      callback(null, {
+        statusCode: e.statusCode || 500,
+        body: 'Error: Could not find Users: ' + e
+      })
+    })
+
+};
+
+module.exports.createUsers = (event,context,callback) => {
+  context.callbackWaitsForEmptyEventLoop = false;
+
+  const data = JSON.parse(event.body);
+  db.insert('users', data)
+    .then(res => {
+      callback(null,{
+        statusCode: 200,
+        body: "User Created! " + res
+      })
+    })
+    .catch(e => {
+      callback(null,{
+        statusCode: e.statusCode || 500,
+        body: "Could not create User " + e
+      })
+    }) 
+};
+
+module.exports.updateUsers = (event,context,callback) => {
+  context.callbackWaitsForEmptyEventLoop = false;
+
+  const data = JSON.parse(event.body);
+  db.updateById('users', event.pathParameters.id, data)
+    .then(res => {
+      callback(null,{
+        statusCode: 200,
+        body: "User Updated!" + res
+      })
+    })
+    .catch(e => {
+      callback(null,{
+        statusCode: e.statusCode || 500,
+        body: "Could not update User" + e
+      })
+    }) 
+};
+
+module.exports.deleteUsers = (event,context,callback) => {
+  context.callbackWaitsForEmptyEventLoop = false;
+
+  db.deleteById('users', event.pathParameters.id)
+    .then(res => {
+      callback(null,{
+        statusCode: 200,
+        body: "User Deleted!"
+      })
+    })
+    .catch(e => {
+      callback(null,{
+        statusCode: e.statusCode || 500,
+        body: "Could not delete User" + e
+      })
+    }) 
+};
